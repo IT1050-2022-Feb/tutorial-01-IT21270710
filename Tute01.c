a007ae158fb01b1df8e0fd6028e093e8d5ca906e
W.A.I.A.BANDARA <99311591+IT21270710@users.noreply.github.com>
2022-02-09 06:10:16 +0000

    Completed 1

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+  int mark1, mark2;
+  double avg=0, total=0;
+  printf("Enter the mark 1 : ");
+  scanf("%d", &mark1);
+
+  printf("Enter the mark 2 : ");
+  scanf("%d", &mark2);
+
+  total = mark1 + mark2;
+  avg = total/2;
+
+  printf("Average of two marks : %.2f", avg);
+
   
   return 0;
 }
